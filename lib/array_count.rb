816298acbdfc9d06637a48487c96feb9d7169fd1
Steven Williams <evilgeniusnyc@me.com>
2020-07-22 09:27:00 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -11,6 +11,6 @@ end
 
 def count_empty_strings(array)
     array.count do |element|
-      element == "" 
+      element == ""  # alternative solution: element.class == String && element.empty?
     end
 end
